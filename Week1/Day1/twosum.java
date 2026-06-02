78f5d7aaf07d9171244dfa9d9542eee0d987fb7c
Dakshvarshney03 <vdaksh311@gmail.com>
2026-06-02 23:12:57 +0530

    Create twosum.java

---
+++ Week1/Day1/twosum.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int i =0;i<n;i++){
+            int a = nums[i];
+            int more = target - a;
+            if(map.containsKey(more)){
+                return new int[]{map.get(more),i};
+            }
+            map.put(a,i);
+        }
+        return new int[]{-1,1};
+        
+    }
+}
